d10f75480f6ecc97e786b1c224461b66dd0e7dff
unknown <jterweeme@gmail.com>
2014-06-05 14:14:40 +0200

    backup

--- od/main.cpp
+++ od/main.cpp
@@ -1,9 +1,40 @@
 #include <fstream>
 #include <iostream>
+#include <stdint.h>
+
+#ifdef _WIN32
+#include <io.h>
+#include <fcntl.h>
+#endif
+
+class Hex
+{
+public:
+    static void dump(std::istream &input);
+};
+
+void Hex::dump(std::istream &input)
+{
+    uint8_t arr[17] = { 0 };
+
+    while (!input.eof())
+    {
+        input.read((char *)arr, 16);
+
+        if (input.gcount() <= 0)
+            break;
+
+        std::cout << std::hex;
+        std::cout << (int)arr << std::endl;
+    }
+}
 
 int main(int argc, char **argv)
 {
-    std::cout << "Onzin" << std::endl;
+#ifdef _WIN32
+    _setmode(_fileno(stdin), O_BINARY);
+#endif
+    Hex::dump(std::cin);
     return 0;
 }
 
