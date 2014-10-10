df70a5e13197efb5ea892589da57503ef59705c8
Jasper ter Weeme <jasper@nbalternate.lan>
2014-10-10 03:54:58 +0200

    backup

--- dd/dd.cpp
+++ dd/dd.cpp
@@ -2,28 +2,33 @@
 #include <iostream>
 #include <sstream>
 #include <cstring>
+#include <stdint.h>
 using namespace std;
 
 class Options
 {
-    string _if;
-    string _of;
+    const char *_if;
+    const char *_of;
 public:
     int parse(int argc, char **argv);
     void dump(ostream &os);
+    const char * const ifs() { return _if; }
+    const char * const ofs() { return _of; }
     string toString() { ostringstream oss; dump(oss); return oss.str(); }
 };
 
 class App
 {
     Options _options;
+    char *_buf;
 public:
     int run(int argc, char **argv);
 };
 
 void Options::dump(ostream &os)
 {
-    os << "Input file: " << _if;
+    os << "Input file: " << _if << "\n";
+    os << "Output file: " << _of;
 }
 
 int Options::parse(int argc, char **argv)
@@ -31,7 +36,10 @@ int Options::parse(int argc, char **argv)
     for (int i = 1; i < argc; i++)
     {
         if (strncmp(argv[i], "if", 2) == 0)
-            _if = string(argv[i] + 3);
+            _if = argv[i] + 3;
+
+        if (strncmp(argv[i], "of", 2) == 0)
+            _of = argv[i] + 3;
 
         
     }
@@ -43,6 +51,16 @@ int App::run(int argc, char **argv)
     _options.parse(argc, argv);
     _options.dump(cout);
     cout << "\n";
+    _buf = new char[512];
+    ifstream ifs(_options.ifs());
+    ofstream ofs(_options.ofs());
+
+    while (ifs)
+    {
+        ifs.read((char *)_buf, 512);
+        ofs.write(_buf, ifs.gcount());
+    }
+
     return 0;
 }
 
