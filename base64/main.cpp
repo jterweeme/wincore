cdcab7ae22c683bce7cff3924ba3332d3ea4c23a
Jasper ter Weeme <jasper@nbalternate.lan>
2014-11-04 00:03:58 +0100

    char moest unsigned zijn!

--- base64/main.cpp
+++ base64/main.cpp
@@ -17,16 +17,18 @@ void App::convert(istream &is, ostream &os)
 {
     for (int i = 1; is; i++)
     {
-        char buf[3] = {0};
-        is.read(buf, sizeof(buf));
+        uint8_t buf[3] = {0};
+        is.read((char *)buf, sizeof(buf));
 
         if (is.gcount() <= 0)
             break;
 
+        uint8_t s2 = (buf[1] >> 4 | buf[0] << 4) & 0x3f;
+        uint8_t s3 = ((buf[1] << 2)  | (buf[2] >> 6)) & 0x3f;
         uint8_t remainder = is.gcount() % 3;
-        os << set[buf[0] >> 2];
-        os << set[(buf[1] >> 4 | buf[0] << 4) & 0x3f];
-        os << (remainder == 1 ? '=' : set[(buf[1] << 2 | buf[2] >> 6) & 0x3f]);
+        os << set[(buf[0] >> 2) & 0x3f];
+        os << set[s2];
+        os << (remainder == 1 ? '=' : set[s3]);
         os << (remainder != 0 ? '=' : set[buf[2] & 0x3f]);
 
         if (i % 19 == 0)
